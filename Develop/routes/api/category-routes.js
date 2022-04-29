d32978f70a5b754b347d9d95ab0ea67d3f327184
Elizabeth <ekkjohnson@gmail.com>
2022-04-28 19:31:03 -0500

    delete category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -8,10 +8,10 @@ router.get('/', (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product}]
+      include: [{ model: Product }]
     });
     res.status(200).json(categoryData);
-  }catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
@@ -25,46 +25,60 @@ router.get('/:id', (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'no category found based on this ID'});
+      res.status(404).json({ message: 'no category found based on this ID' });
       return;
     }
 
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
-  } 
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
-  try{
-    const categoryData = await Category.create( {
+  try {
+    const categoryData = await Category.create({
       category_name: req.body.category_name,
     });
     res.status(200).json(categoryData)
-  }catch (err) {
+  } catch (err) {
     res.status(400).json(err);
   }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  try{
-    const categoryData= await Category.update(
+  try {
+    const categoryData = await Category.update(
       req.body, {
-        where: {
-          id:req.params.id,
-        },
-    
+      where: {
+        id: req.params.id,
+      },
+
     });
     res.status(200).json(categoryData)
-  }catch(err) {
+  } catch (err) {
     res.status(400).json(err);
   }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ mesage: 'no category matches this ID' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
