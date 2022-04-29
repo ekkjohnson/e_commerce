11e9bf7c5c24ec8b74fd5f1e80fb59ba1d0ab300
Elizabeth <ekkjohnson@gmail.com>
2022-04-29 09:14:21 -0500

    tag routes added

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,8 +5,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async(req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [{ model: Category}, {model: Tag}]
@@ -15,19 +13,20 @@ router.get('/', async(req, res) => {
   }catch (err) {
     res.status(500).json(err);
   }
+  // find all products
+  // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  router.get('/:id', async (req, res) => {
-    try{
-      const productData = await Product.findByPk(req.params.id, {
-        include: [{ model: Category}, {model: Tag}]
-      });
-      res.status(200).json(productData);
-    }catch (err) {
-      res.status(500).json(err);
-    }
+router.get('/:id', async (req, res) => {
+  try{
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, {model: Tag}]
+    });
+    res.status(200).json(productData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
