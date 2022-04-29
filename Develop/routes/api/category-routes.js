1da3cb8e771d52bb5a9bb03eca3a4cc9087ad83c
Elizabeth <ekkjohnson@gmail.com>
2022-04-28 19:29:22 -0500

    new and id category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -37,10 +37,30 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create( {
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData)
+  }catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try{
+    const categoryData= await Category.update(
+      req.body, {
+        where: {
+          id:req.params.id,
+        },
+    
+    });
+    res.status(200).json(categoryData)
+  }catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
